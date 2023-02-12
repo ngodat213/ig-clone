b54d3b407f8a6dca51c7c83649c8bd95cf08fcf3
HydraCoder <ngodat.it213@gmail.com>
2023-02-12 23:01:09 +0700

    update

---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login || Instagram</title>
+    <link rel="shortcut icon" href="public/assets/images/favicon/instagram.ico" type="image/x-ixon">
+</head>
+<section class="pageContainer">
+    <main class="row">
+        <div class="here__image">
+
+        </div>
+        <article class="col-2">
+            <form action="">
+                <div class="siteLogoContainer">
+                    <img src="public/assets/images/logo/instagram.png" alt="Instagram Logo">
+                    <input type="text" placeholder="Email or Username" class="form--input" name="email_username">
+                    <div class="passwordContainer">
+                        <input type="password" placeholder="Password" class="form--input" name="password">
+                        <span class="show_hide_text cursor-pointer" id="show-hide-password"></span>
+                    </div>
+                    <button class="button cursor-pointer" type="submit" name="submitButton">Login</button>
+                </div>
+            </form>
+        </article>
+    </main>
+</section>
+
+<body>
+
+</body>
+
+</html>
