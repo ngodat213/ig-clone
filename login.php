9fc88f29791bd1c1254e93167d86de8af9b09467
HydraCoder <ngodat.it213@gmail.com>
2023-02-15 18:48:43 +0700

    update

--- login.php
+++ login.php
@@ -7,24 +7,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login || Instagram</title>
     <link rel="shortcut icon" href="public/assets/images/favicon/instagram.ico" type="image/x-ixon">
+    <link rel="stylesheet" href="public/css/register.css">
 </head>
 <section class="pageContainer">
     <main class="row">
-        <div class="here__image">
-
+        <div class="col-1">
+            <div class="heroImage"></div>
         </div>
         <article class="col-2">
             <form action="">
                 <div class="siteLogoContainer">
                     <img src="public/assets/images/logo/instagram.png" alt="Instagram Logo">
-                    <input type="text" placeholder="Email or Username" class="form--input" name="email_username">
-                    <div class="passwordContainer">
-                        <input type="password" placeholder="Password" class="form--input" name="password">
-                        <span class="show_hide_text cursor-pointer" id="show-hide-password"></span>
-                    </div>
-                    <button class="button cursor-pointer" type="submit" name="submitButton">Login</button>
                 </div>
+                <input type="text" placeholder="Email or Username" class="form--input" name="email_username">
+                <div class="passwordContainer">
+                    <input type="password" placeholder="Password" class="form--input" name="password">
+                    <span class="show_hide_text cursor-pointer" id="show-hide-password">Show</span>
+                </div>
+                <button class="button cursor-pointer" type="submit" name="submitButton">Login</button>
+                <span class="separator">Or</span>
+                <a href="#" class="password_reset">Forget Password</a>
             </form>
+            <footer class="form--footer">
+                Don't hava an account <a href="">Sign Up</a>
+            </footer>
         </article>
     </main>
 </section>
